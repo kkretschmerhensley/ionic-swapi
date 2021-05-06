ad742222ee5a5e1bf37f1e9f001733a184d069fd
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-05-06 15:52:00 -0500

    API call to SW planets is working

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { merge, race, EMPTY } from "rxjs";
+import { expand, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadPlanets() {
+    // return this.http.get("http://swapi.dev/api/planets/");
+
+    const pageOne = this.http.get("http://swapi.dev/api/planets/");
+    // const pageTwo = this.http.get("http://swapi.dev/api/planets/?page=2");
+    //
+    // return race(
+    //   pageOne, pageTwo
+    // );
+
+    return pageOne.pipe(
+      tap(x => console.log("tap", x)),
+      expand(
+        x => (x as any).next ? this.http.get((x as any).next) : EMPTY
+      )
+    );
+
+  }
 }
